3fd1e00a504eb66edb0016d2611bab824c5d7e58
Shabrya <shabryalott@Shabryas-MacBook-Pro.local>
2016-11-09 10:14:46 -0800

    Midterm

---
+++ Midterm/TivaC_Midterm/midterm/Debug/main.d
@@ -0,0 +1,47 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_i2c.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_ints.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_types.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/i2c.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/pin_map.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/rom_map.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_gpio.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/uart.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/utils/uartstdio.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/utils/uartstdio.c
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_uart.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/debug.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/interrupt.h
+main.obj: C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/rom.h
+
+../main.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_i2c.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_ints.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_types.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/i2c.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/pin_map.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/rom_map.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_gpio.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/uart.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/utils/uartstdio.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/utils/uartstdio.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/inc/hw_uart.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/debug.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/interrupt.h: 
+C:/ti/ccsv6/TivaWare_C_Series-2.1.3.156/driverlib/rom.h: 
